0dce49d8cb5a341a0a900991b31a12ef9dfd8666
RachelVB <vanbuskirk2014@hotmail.com>
2019-06-23 20:41:55 -0700

    Carosuel fix

--- js/main-home.js
+++ js/main-home.js
@@ -1,4 +1,12 @@
 $(document).ready(() => {
 
   
+  
 })
+
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+  mainNav.classList.toggle('active');
+});
