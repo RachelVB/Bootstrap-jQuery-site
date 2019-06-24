e6763f1dbbb7eaafc6c1e4e76685a2976f729f64
RachelVB <vanbuskirk2014@hotmail.com>
2019-06-24 08:23:21 -0700

    add jquery

--- js/main-home.js
+++ js/main-home.js
@@ -1,12 +1,26 @@
 $(document).ready(() => {
 
+  $('.nav-links').on('mouseenter', event => {
+    $(event.currentTarget).addClass('active-nav')
+  }).on('mouseleave', event => {
+    $(event.currentTarget).removeClass('active-nav')
+  })
+
+  /* Iphone size */
+  $(window).resize(function () { 
+
+    if ($('header').width() <= 500 ) {
+      $('nav').remove('.main-nav');
+    }
+    
+  });
   
   
-})
-
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
-
-navBarToggle.addEventListener('click', function () {
-  mainNav.classList.toggle('active');
 });
+
+// let mainNav = document.getElementById('js-menu');
+// let navBarToggle = document.getElementById('js-navbar-toggle');
+
+// navBarToggle.addEventListener('click', function () {
+//   mainNav.classList.toggle('active');
+// });
