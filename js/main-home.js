268241d511d543ca9e7f962602ca60a08d621f3a
RachelVB <vanbuskirk2014@hotmail.com>
2019-06-25 07:56:08 -0700

    jquery modify for mobile

--- js/main-home.js
+++ js/main-home.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(() => {
 
   $('.nav-links').on('mouseenter', event => {
@@ -6,17 +8,42 @@ $(document).ready(() => {
     $(event.currentTarget).removeClass('active-nav')
   })
 
-  /* Iphone size */
-  $(window).resize(function () { 
+$('.logo').on('click', () => {
 
-    if ($('header').width() <= 500 ) {
-      $('nav').remove('.main-nav');
-    }
-    
-  });
-  
+  $('.main-nav').toggleClass('mobile-nav');
+
+ 
+})
+
+
+
+  // $(window).on("resize", function (e) {
+  //   checkScreenSize();
   
 });
+
+// checkScreenSize();
+
+
+// function checkScreenSize(){
+//   let newWindowWidth = $(window).width();
+//   if (newWindowWidth < 800) {
+//     $('.main-nav').addClass('nav-linkshide');
+//   }
+// }
+// });
+
+
+
+/* Iphone size */
+// $(window).resize(function () { 
+
+//   if ($('header').width() <= 800 ) {
+//     $('.main-logo').removeClass('logo');
+//     $('.main-nav').addClass('nav-linkshide');
+//   }
+  
+// });
 
 // let mainNav = document.getElementById('js-menu');
 // let navBarToggle = document.getElementById('js-navbar-toggle');
