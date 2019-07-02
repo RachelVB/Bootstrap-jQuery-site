b1f792580592ebb39aef98dce8632cdf4b74e63f
RachelVB <rachelvanbus@hotmail.com>
2019-07-02 15:40:33 -0700

    fix jquery

---
+++ js/main-homePortfolio.js
@@ -0,0 +1,30 @@
+
+$(document).ready(() => {
+
+  $('.nav-links').on('mouseenter', event => {
+    $(event.currentTarget).addClass('active-nav')
+  }).on('mouseleave', event => {
+    $(event.currentTarget).removeClass('active-nav')
+  })
+
+  $('.card.mb-4').on('mouseenter', event => {
+    $(event.currentTarget).animate({
+      bottom: "+=50",
+      opacity: 3
+  }, 300)
+    }).on('mouseleave', event => {
+    $(event.currentTarget).animate({
+      bottom: "-=50",
+      opacity: 0.30
+  }, 300)
+
+    })
+
+    // $('.featurette').on('mouseover', event => {
+//   $(event.currentTarget).addClass('feature-color').fadeIn(100)
+//   }).on('mouseleave', event => {
+//     $(event.currentTarget).removeClass('feature-color').fadeOut
+// })
+  
+
+})
