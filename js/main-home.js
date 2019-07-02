3a698222edf5d686a75cf52451e9c1449add3d51
RachelVB <rachelvanbus@hotmail.com>
2019-07-02 13:47:47 -0700

    jquery fix animate arrows

--- js/main-home.js
+++ js/main-home.js
@@ -2,18 +2,87 @@
 
 $(document).ready(() => {
 
-  $('.nav-links').on('mouseenter', event => {
+
+    $('div.col-md-7.hide').hide();
+    $('div.col-md-7.hide1').hide();
+    $('div.col-md-7.hide2').hide();
+
+  
+  
+  
+    $('.nav-links').on('mouseenter', event => {
     $(event.currentTarget).addClass('active-nav')
   }).on('mouseleave', event => {
     $(event.currentTarget).removeClass('active-nav')
   })
 
-$('.logo').on('click', () => {
-
-  $('.main-nav').toggleClass('mobile-nav');
-
+// $('.logo').on('click', () => {
  
-})
+// })
+
+
+
+
+$('.icon-container').on('click', () => {
+  $('.icon-container').animate({
+    paddingLeft: '20rem'
+   })
+
+   $('.hide').show("fold", 4000);
+
+   
+ });
+ /* .on('mouseleave', () => {
+   $('.hide').fadeOut('slow')
+ }) */
+
+ $('.icon-containerone').on('click', () => {
+  $('.icon-containerone').animate({
+    paddingLeft: '20rem'
+   })
+
+   $('.hide1').show("fold", 4000);
+
+   
+ });
+ /* .on('mouseleave', () => {
+  $('.hide1').fadeOut('slow')
+}) */
+
+$('.icon-containertwo').on('click', () => {
+  $('.icon-containertwo').animate({
+    paddingLeft: '20rem'
+   })
+
+   $('.hide2').show("fold", 4000);
+
+   
+ });
+ /* .on('mouseleave', () => {
+  $('.hide2').fadeOut('slow')
+}) */
+
+
+
+  // $('.featurette').on('mouseenter', () => {
+  // $(this).next('.col-md-7').removeClass(".hide")
+  // }).on('mouseleave', () => {
+  //   $(this).next('.col-md-7').addClass(".hide")
+  // })
+  
+  
+
+
+
+// $('.featurette').on('mouseenter', () => {
+//   $('div.col-md-7').show();
+// })
+
+// $('.featurette').on('mouseover', event => {
+//   $(event.currentTarget).addClass('feature-color').fadeIn(100)
+//   }).on('mouseleave', event => {
+//     $(event.currentTarget).removeClass('feature-color').fadeOut
+// })
 
 
 
